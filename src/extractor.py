da10eb57c2ad1f5bf5108857e6b887540a83165e
gpratoe <gaston.prato@mi.unc.edu.ar>
2023-11-29 17:50:36 -0300

    extractor class

---
+++ src/extractor.py
@@ -0,0 +1,98 @@
+import re
+import requests
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+import execjs
+
+class Extractor:
+    def __init__(self, videoId):
+        self.videoId = videoId
+        self.formats = self._get_video_formats(videoId)
+        self.playerUrl = self._get_player_url(videoId)
+        self.challenge = self._get_challenge(videoId)
+    def _get_video_formats(self, videoId):
+        url = f'https://www.youtube.com/youtubei/v1/player'
+        data = {
+            "videoId": videoId,
+            "context": {
+                "client": {
+                    "clientName": "MWEB",
+                    "clientVersion": "2.20231129.01.00"
+                }
+            }
+        }
+        response = requests.post(url, json=data)
+        formats = response.json()['streamingData']['adaptiveFormats']
+
+        return formats
+
+    def _get_player_url(self, videoId):
+        response = requests.get(f'https://www.youtube.com/embed/{videoId}')
+
+        pattern = r'\/s\/player\/(\w+)\/player_ias.vflset\/\w+\/base.js'
+        match = re.search(pattern, response.text)
+        player_hash = match.group(1)
+        return f'https://www.youtube.com/s/player/{player_hash}/player_ias.vflset/en_US/base.js'
+
+    def _get_challenge(self, videoId):
+        playerUrl = self._get_player_url(videoId)
+
+        response_txt = requests.get(playerUrl).text
+
+        challenge_name = re.search(r'\.get\("n"\)\)&&\(b=([a-zA-Z0-9$]+)(?:\[(\d+)\])?\([a-zA-Z0-9]\)', response_txt).group(1)
+        challenge_name = re.search(r'var {}\s*=\s*\[(.+?)\]\s*[,;]'.format(challenge_name), response_txt).group(1)
+
+        challenge_pattern = r'{}\s*=\s*function\s*\(([\w$]+)\)\s*{{(.+?)\s*return\s*[\w$]+\.join\(""\)}};'.format(challenge_name)
+        challenge = re.search(challenge_pattern, response_txt, re.DOTALL).group(0)
+
+        challenge = re.sub(r'^\w+\s*=\s*', '', challenge)
+        challenge = re.sub(r';$', '', challenge)
+
+        return challenge
+    def parse_formats(self):
+        res = "{:<3} | {:<10} | {:<5}".format("TAG", "FORMAT", "QUALITY") + "\n"
+        res += "{:<3}|{:<10}|{:<5}".format("----","------------","--------") + "\n"
+        for format in self.formats:
+            itag = str(format['itag'])
+            mimeType = str(format['mimeType'])
+            mediaFormat = mimeType.split(";")[0]
+
+            qualityLabel = (format['qualityLabel']
+                              if mediaFormat.split("/")[0] == "video" 
+                              else "")
+
+            audioQuality = (format['audioQuality'].removeprefix("AUDIO_QUALITY_").lower() 
+                             if mediaFormat.split("/")[0] == "audio" 
+                             else "")
+
+
+            formatedLine = "{:<3} | {:<10} | {:<5}".format(itag,
+                                                               mediaFormat,
+                                                                 qualityLabel if mediaFormat.split("/")[0] == "video" else audioQuality)
+
+            res += formatedLine + '\n'
+        return res
+    
+    def _get_format_info(slef, tag):
+        return next(info for info in slef.formats if info['itag'] == tag)
+
+    def get_format_url(self, tag):
+        formatInfo = self._get_format_info(tag)
+        url = formatInfo['url']
+        return url
+    
+    def solve_challenge(self, formatUrl):
+    
+        parsed_url = urlparse(formatUrl)
+
+        query_params = parse_qs(parsed_url.query)
+
+        n = query_params['n'][0]
+
+        n_transformed = execjs.eval(f"(({self.challenge})(String('{n}')).split)('{{ return a; }}')")
+
+        query_params['n'][0] = n_transformed
+
+        encoded_query_params = urlencode(query_params, doseq=True)
+        updated_url = urlunparse(parsed_url._replace(query=encoded_query_params))    
+
+        return updated_url
--- src/main.py
+++ src/main.py
@@ -1,90 +1,77 @@
 import requests
 import sys
 import json
-
-#   {
-#     "itag": 251,
-#     "url": "https://rr1---sn-p2a5o-jvgl.googlevideo.com/videoplayback?expire=1701201219&ei=4_BlZf2GLMDZ1sQPwJGfyA8&ip=2800%3A40%3A37%3A42b%3Aa022%3Ab1c4%3Ac9b7%3A21b0&id=o-AN5UunsMM22un0V1VEI67AAXM3mEn_youtfIe7Ll8hyI&itag=251&source=youtube&requiressl=yes&mh=Qf&mm=31%2C29&mn=sn-p2a5o-jvgl%2Csn-x1x7dn7s&ms=au%2Crdu&mv=m&mvi=1&pl=48&initcwndbps=202500&spc=UWF9f7qV20wgghjtPiS3_bkmKPFp4_H7cO_qsQsnow&vprv=1&svpuc=1&mime=audio%2Fwebm&ns=DgxpX_8c4qzbggrb0Tf5wE8P&gir=yes&clen=5772821&dur=380.041&lmt=1700883937971314&mt=1701179126&fvip=1&keepalive=yes&fexp=24007246&c=WEB&txp=5532434&n=ooa6S2sRXnZF4YQo&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&sig=ANLwegAwRQIgOd7Kc1r4M4-iZSYuucNw_2s0oyxMOMVbLQ0VBzuS_-gCIQCEubrlavwMyP-q6LkVSuoVRYC4p8h8ldAPtnrxc-jm8A%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AM8Gb2swRgIhAMYa9ue-vzRlM4XJERfhIa-1ZHyPNXG7JdxuWiq_fr8lAiEA8Tcodh00ddY0kf4f7sTjeDQ_kiANwfQsgGv6PVA4vZ8%3D",
-#     "mimeType": "audio/webm; codecs=\"opus\"",
-#     "bitrate": 209600,
-#     "initRange": {
-#       "start": "0",
-#       "end": "265"
-#     },
-#     "indexRange": {
-#       "start": "266",
-#       "end": "925"
-#     },
-#     "lastModified": "1700883937971314",
-#     "contentLength": "5772821",
-#     "quality": "tiny",
-#     "projectionType": "RECTANGULAR",
-#     "averageBitrate": 121519,
-#     "audioQuality": "AUDIO_QUALITY_MEDIUM",
-#     "approxDurationMs": "380041",
-#     "audioSampleRate": "48000",
-#     "audioChannels": 2,
-#     "loudnessDb": -3.8199997
-#   }
-
+from tqdm import tqdm
+import time
+import os
+import re
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+import execjs
+from extractor import Extractor
 
 script_name = sys.argv[0]
 videoId = sys.argv[1]
 
-def get_video_formats(videoId):
-    url = f'https://www.youtube.com/youtubei/v1/player'
-    data = {
-        "videoId": videoId,
-        "context": {
-            "client": {
-                "clientName": "WEB",
-                "clientVersion": "2.20230810.05.00"
-            }
-        }
-    }
-    response = requests.post(url, json=data)
-    formats = response.json()['streamingData']['adaptiveFormats']
-    return formats
 
-def parse_formats(formats):
-    res = "{:<3} | {:<10} | {:<5}".format("TAG", "FORMAT", "QUALITY") + "\n"
-    res += "{:<3}|{:<10}|{:<5}".format("----","------------","--------") + "\n"
-    for format in formats:
-        
-        itag = str(format['itag'])
-        mimeType = str(format['mimeType'])
-        mediaFormat = mimeType.split(";")[0]
-        
-        qualityLabel = (format['qualityLabel']
-                          if mediaFormat.split("/")[0] == "video" 
-                          else "")
-        
-        audioQuality = (format['audioQuality'].removeprefix("AUDIO_QUALITY_").lower() 
-                         if mediaFormat.split("/")[0] == "audio" 
-                         else "")
-        
 
-        formatedLine = "{:<3} | {:<10} | {:<5}".format(itag,
-                                                           mediaFormat,
-                                                             qualityLabel if mediaFormat.split("/")[0] == "video" else audioQuality)
-        
-        res += formatedLine + '\n'
-    return res
 
-def download_video(itagv, itaga, formats):
-    videoUrl = next(info['url'] for info in formats if info['itag'] == itagv)
-    audioUrl = next(info['url'] for info in formats if info['itag'] == itaga)
-    print(f'vidurl:{videoUrl}')
-    print(f'audurl:{audioUrl}')
-    pass
+def download_file(url, filename):
+    for tries in range(1,4):
+        try:
+            fileExists = os.path.exists(filename)
+            
+            downloadedRange = os.path.getsize(filename) if fileExists else 0
+
+            rangeHeader = {'Range': f'bytes={downloadedRange}-'} if fileExists else {}
+
+            with requests.get(url, headers=rangeHeader, stream=True) as response:
+                response.raise_for_status()
+
+                total_size = int(response.headers.get('content-length', 0))
+                
+                with open(filename, 'wb') as local_file, tqdm(
+                    desc= f'Downloading {filename}',
+                    total=total_size,
+                    unit='B',
+                    unit_scale=True,
+                    unit_divisor=1024,
+                    initial=downloadedRange
+                ) as progressbar:
+                    
+                    for chunk in response.iter_content(chunk_size=8192):
+                        if chunk:
+                            local_file.write(chunk)
+                            progressbar.update(len(chunk))
+                
+                print(f'Download completed: {filename}')
+                break
+        except requests.exceptions.RequestException as e:
+            
+            print(f"Error during download: {e}")
+            if tries < 3:
+                print(f"Retrying in 3 seconds...")
+                time.sleep(3)
+
+
+
+
+def download_video(video_url, audio_url):
+    download_file(video_url, "video_out")
+    download_file(audio_url, "audio_out")
+    # todo: merge audio and video
 
 def main():
-    formats = get_video_formats(videoId)
-    res = parse_formats(formats)
+    ext = Extractor(videoId)
+    
+    res = ext.parse_formats()
     print(res)
 
-    itagv = int(input("ingrese tag de video: "))
-    itaga = int(input("ingrese tag de audio: "))
-    download_video(itagv,itaga,formats)
+    tag_video = int(input("ingrese tag de video: "))
+    tag_audio = int(input("ingrese tag de audio: "))
+    
+    video_url = ext.get_format_url(tag_video)
+    audio_url = ext.get_format_url(tag_audio)
+    
+    download_video(ext.solve_challenge(video_url), ext.solve_challenge(audio_url))
 
-main() 
+main()
